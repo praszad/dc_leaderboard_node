48b5e1a6c1169da2ccd05a8d525252c5777d4c5e
Prasad Babu Selvaraj <prasadbabu.s@contu.in>
2020-02-28 07:25:02 +0530

    Dist Handle on develop

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _server = _interopRequireDefault(require("./server"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _server.default)();
--- dist/routers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _PublicRoutes = _interopRequireDefault(require("./PublicRoutes"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = {
-  PublicRouter: _PublicRoutes.default
-};
